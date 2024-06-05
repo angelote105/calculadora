b99aa147160f85a3fc8cd4078a31c177242c91eb
a.teran <a.teran@PYD07ZYD6-AECIT>
2024-06-05 15:17:17 -0500

    final

--- calculadora/vista/vista1.xaml.cs
+++ calculadora/vista/vista1.xaml.cs
@@ -2,17 +2,15 @@ namespace calculadora.vista;
 
 public partial class vista1 : ContentPage
 {
-	public vista1()
-	{
-		InitializeComponent();
-	}
+    public vista1()
+    {
+        InitializeComponent();
+    }
 
     private void btnCalcular_Clicked(object sender, EventArgs e)
     {
-
         if (pkAlumnos.SelectedIndex < 0)
         {
-
             DisplayAlert("Alerta", "Seleccione 1", "Ok");
         }
         else
@@ -20,53 +18,56 @@ public partial class vista1 : ContentPage
             var datos = pkAlumnos.Items[pkAlumnos.SelectedIndex];
             string datos1 = datos.ToString();
 
-            var ns1 = double.Parse(txtNS1.Text);
-            var ne1 = double.Parse(txtNE1.Text);
-            var ns2 = double.Parse(txtNS2.Text);
-            var ne2 = double.Parse(txtNE2.Text);
+            double ns1 = double.Parse(txtNS1.Text);
+            double ne1 = double.Parse(txtNE1.Text);
+            double ns2 = double.Parse(txtNS2.Text);
+            double ne2 = double.Parse(txtNE2.Text);
             string fecha = dpFecha.Date.ToString();
 
             double np1 = (ns1 * 0.3) + (ne1 * 0.2);
-
             double np2 = (ns2 * 0.3) + (ne2 * 0.2);
 
             double nf = np1 + np2;
 
-            
+            if (nf > 0 && nf < 5)
+            {
+                string msm = ("Alumno : " + datos1 + "\n"
+                            + "Fecha : " + fecha + "\n"
+                            + "Nota parcial 1: " + np1 + "\n"
+                            + "Nota parcial 2: " + np2 + "\n"
+                            + "Nota final: " + nf + "\n"
+                            + "Reprobado");
 
-            if (nf > 0 && nf < 5) {
-                string msm = ("Alumno : " + datos1 + "\n"
-                    + "fecha : " + fecha + "\n"
-                    + "nota parcial 1" + np1 + "\n"
-                    + "nota parcial 2" + np2 + "\n"
-                    + "nota final : "+nf +"\n"
-                    + "REPROBADO");
+                DisplayAlert("Estimado: ", msm, "Ok");
             }
-            if (nf > 5 && nf < 7)
+            else if (nf >= 5 && nf < 7)
             {
                 string msm = ("Alumno : " + datos1 + "\n"
-                    + "fecha : " + fecha + "\n"
-                    + "nota parcial 1" + np1 + "\n"
-                    + "nota parcial 2" + np2 + "\n"
-                    + "nota final : " + nf + "\n"
-                    + "COMPLEMENTARIO");
+                            + "Fecha : " + fecha + "\n"
+                            + "Nota parcial 1: " + np1 + "\n"
+                            + "Nota parcial 2: " + np2 + "\n"
+                            + "Nota final: " + nf + "\n"
+                            + "Complementario");
+
+                DisplayAlert("Estimado: ", msm, "Ok");
             }
-            if (nf > 7 && nf < 11)
+            else if (nf >= 7 && nf <= 10)
             {
                 string msm = ("Alumno : " + datos1 + "\n"
-                    + "fecha : " + fecha + "\n"
-                    + "nota parcial 1" + np1 + "\n"
-                    + "nota parcial 2" + np2 + "\n"
-                    + "nota final : " + nf + "\n"
-                    + "APROBADO ");
+                            + "Fecha : " + fecha + "\n"
+                            + "Nota parcial 1: " + np1 + "\n"
+                            + "Nota parcial 2: " + np2 + "\n"
+                            + "Nota final: " + nf + "\n"
+                            + "Aprobado");
+
+                DisplayAlert("Estimado: ", msm, "Ok");
             }
             else
             {
-                DisplayAlert("Alerta", "revisa codigo", "ok");
+                DisplayAlert("Alerta", "Revisa codigo", "Ok");
             }
-
-
         }
 
     }
+    
 }
